d77c2cfbeb5ac50ab12d8969306342c2119246a8
rishi557 <74720468+rishi557@users.noreply.github.com>
2021-04-27 18:00:22 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# proc-37
+# CarRacingGame1.0
+Car Racing Game Stage 1.0
---
+++ sketch.js
@@ -0,0 +1,18 @@
+var canva;
+
+function setup(){
+  canvas = createCanvas(850,400);
+}
+
+
+function draw(){
+  background("pink");
+  drawSprites
+  
+}
+display();
+this.title.html("myquizgame");
+this.title.position(350,0);
+
+this.question.html("Question:- What starts and ends with the letter 'E',but has only one letter?");
+this.question.position(150,80);
